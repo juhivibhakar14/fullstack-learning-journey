cc85a7ff58c9b0cbdaf85a11909df7b902a45e42
juhivibhakar14 <juhivibhakar07@gmail.com>
2026-04-14 22:15:56 +0530

    Day 13: Completed more array problems.

---
+++ javascript/ModernArray/findLogicArray.js
@@ -0,0 +1,5 @@
+let arr = [10,20,30,40,50,60,70,80,90];
+
+let result = arr.find(n=> n>20);
+
+console.log(result);
---
+++ javascript/ModernArray/mapConditionArray.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+let result = arr.filter(n => n % 2 === 0);
+
+console.log(result);
