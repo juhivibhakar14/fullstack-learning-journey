f0a6ab09f0b0a38d9f4c211a1abc7d47b016cc84
juhivibhakar14 <juhivibhakar07@gmail.com>
2026-05-06 06:13:03 +0530

    Created basic counter app and todo app.

---
+++ javascript/DOMBasic/ToDo/script.js
@@ -0,0 +1,31 @@
+let input = document.getElementById("taskInput");
+let addBtn = document.getElementById("addBtn");
+let list = document.getElementById("list");
+
+addBtn.addEventListener("click", () => {
+  let task = input.value;
+
+  if (task === "") return;
+
+  // create li
+  let li = document.createElement("li");
+  li.textContent = task;
+
+  // create delete button
+  let delBtn = document.createElement("button");
+  delBtn.textContent = " X";
+
+  // delete logic
+  delBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  // add button to li
+  li.appendChild(delBtn);
+
+  // add li to list
+  list.appendChild(li);
+
+  // clear input
+  input.value = "";
+});
